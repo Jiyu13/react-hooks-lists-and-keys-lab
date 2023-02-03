4440d58dfd6b1faa2a71d73384a5bddc914e41c0
Ziru Zou <jiyucoding@yahoo.com>
2023-02-02 20:00:47 -0500

    create an <a> with key prop for each in links array and display each <a>

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,16 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linksList = links.map((link) => {
+    return (
+      <a href={"#" + link} key={link}>{link}</a>
+    )
+  })
+
+  return <nav>
+          {/* display an <a> tag for each link here */}
+          {linksList}
+         </nav>;
 }
 
 export default NavBar;
